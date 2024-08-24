2d063990d5a52f3ec34cab76cfc4ab7026a5fea8
Edwin Peguero <edwinmpeguero@gmail.com>
2024-08-24 17:06:09 -0400

    checkpoint

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import { ReactComponent as PotIcon } from './icon-pot.svg';
-import { ReactComponent as CaretRightIcon} from './icon-caret-right.svg';
+import { ReactComponent as CaretRightIcon } from './icon-caret-right.svg';
+import { ReactComponent as LogoSmallIcon } from './logo-small.svg';
+import { ReactComponent as LogoLargeIcon } from './logo-large.svg';
+import { ReactComponent as OverviewIcon } from './icon-nav-overview.svg';
+import { ReactComponent as TransactionsIcon } from './icon-nav-transactions.svg';
+import { ReactComponent as BudgetsIcon} from './icon-nav-budgets.svg';
+import { ReactComponent as PotsIcon} from './icon-nav-pots.svg';
+import { ReactComponent as RecurringBillsIcon} from './icon-nav-recurring-bills.svg';
+import { ReactComponent as LeftArrowIcon } from './icon-minimize-menu.svg';
 
 type Pot = {
   name: string,
@@ -10,6 +18,30 @@ type Pot = {
 }
 
 function App() {
+  return (
+    <div className="App">
+      <Sidebar/>
+      <Overview/>
+    </div>
+  );
+}
+
+const Sidebar = () => {
+  const [isMaximized, setIsMaximized] = useState(false);
+  return <div className='sidebar' data-maximized={isMaximized}>
+    <LogoSmallIcon className='small-logo'/>
+    <LogoLargeIcon className='large-logo'/>
+    <OverviewIcon className='overview-icon'/>
+    <TransactionsIcon className='transactions-icon'/>
+    <BudgetsIcon className='budgets-icon'/>
+    <PotsIcon className='pots-icon'/>
+    <RecurringBillsIcon className='recurring-bills-icon'/>
+    <MaxMinimizeButton onClick={() => setIsMaximized(!isMaximized)}/>
+  </div>
+}
+
+
+const Overview = () => {
   const pots: Pot[] = [
     {name: 'Savings', color: 'darkgreen', size: 159},
     {name: 'Concert Ticket', color: 'dimgrey', size: 110},
@@ -17,38 +49,34 @@ function App() {
     {name: 'New Laptop', color: 'moccasin', size: 10}
   ];
   return (
-    <div className="App">
-      <div className="grid">
+    <div className="overview">
         <span className="overview-title">Overview</span>
         <div className="figures">
           <Figure className="current-balance" title={"Current Balance"} figure={4836}/>
           <Figure title={"Income"} figure={3814.25}/>
           <Figure title={"Expenses"} figure={1700.50}/>
         </div>
-        <div className="bottom-grid">
-          <Card className="pots">
-            <CardHeader title="Pots" actionButton={<CaretLink title="See Details"/>}/>
-            <Card className="total-saved">
-              <PotIcon/>
-              <span>Total Saved</span>
-              <span className='amount'>{formatCurrency(850, false)}</span>
-            </Card>
-            {pots.map((pot, ix) => 
-              <AmountWithLabel id={`pots-pot${ix+1}`}className={`pot${ix+1}`} label={pot.name} amount={pot.size} color={pot.color}/>
-            )}
+        <Card className="pots">
+          <CardHeader title="Pots" actionButton={<CaretLink title="See Details"/>}/>
+          <Card className="total-saved">
+            <PotIcon/>
+            <span>Total Saved</span>
+            <span className='amount'>{formatCurrency(850, false)}</span>
           </Card>
-          <Card className="transactions">
-            <CardHeader title="Transactions" actionButton={<CaretLink title="View All"/>}/>
-          </Card>
-          <Card className="budgets">
-            <CardHeader title="Budgets" actionButton={<CaretLink title="See Details"/>}/>
-          </Card>
-          <Card className="recurring-bills">
-            <CardHeader title="Recurring Bills" actionButton={<CaretLink title="See Details"/>}/>
-          </Card>
-        </div>
+          {pots.map((pot, ix) => 
+            <AmountWithLabel key={`pots-pot${ix+1}`}className={`pot${ix+1}`} label={pot.name} amount={pot.size} color={pot.color}/>
+          )}
+        </Card>
+        <Card className="transactions">
+          <CardHeader title="Transactions" actionButton={<CaretLink title="View All"/>}/>
+        </Card>
+        <Card className="budgets">
+          <CardHeader title="Budgets" actionButton={<CaretLink title="See Details"/>}/>
+        </Card>
+        <Card className="recurring-bills">
+          <CardHeader title="Recurring Bills" actionButton={<CaretLink title="See Details"/>}/>
+        </Card>
       </div>
-    </div>
   );
 }
 
@@ -79,6 +107,11 @@ const CaretLink = ({title, className}: {title: string} & React.HTMLAttributes<HT
   <div className={`caret-link ${className ?? ''}`}>
     <span>{title}</span>
     <CaretRightIcon/>
+  </div>
+
+const MaxMinimizeButton = ({onClick}: React.ComponentProps<"div">) =>
+  <div className='maximize-button' onClick={onClick}>
+    <LeftArrowIcon/>
   </div>
 
 const formatCurrency = (value: number, cents: boolean = false) =>
